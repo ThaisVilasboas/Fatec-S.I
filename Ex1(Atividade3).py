19452023ca7d7a69f834f135f7d10183b6dfbe74
ThaisVilasboas <144953802+ThaisVilasboas@users.noreply.github.com>
2023-10-07 12:41:27 -0300

    Create Ex1(Atividade3).py

---
+++ Ex1(Atividade3).py
@@ -0,0 +1,15 @@
+#1) Elabore um programa que some 2 NOTAS digitadas pelo usuário.
+#Calcule a média aritmética destas notas e apresente a seguinte
+#mensagem:
+
+notaA = float(input("Digite o valor da sua nota 1:" ))
+notaB = float(input("Digite o valor da sua nota 2:" ))
+
+media = (notaA + notaB) / 2
+if media <4:
+    print("Reprovado")
+elif media <6:
+    print("Exame") 
+else: 
+    print("Aprovado")
+    
