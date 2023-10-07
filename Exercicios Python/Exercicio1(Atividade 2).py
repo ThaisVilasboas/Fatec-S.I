f26a19c4ed79453d19988c12a78636c37dfcd198
ThaisVilasboas <144953802+ThaisVilasboas@users.noreply.github.com>
2023-10-07 09:52:38 -0300

    Create Exercicio1(Atividade 2).py

---
+++ Exercicios Python/Exercicio1(Atividade 2).py
@@ -0,0 +1,9 @@
+#exercicio 1 (Atividade 2) 
+valorDeA=float(input("Digite um valor de A:"))
+valorDeB=float(input("Digite um valor de B:"))
+if(valorDeA > valorDeB):
+    print("Dado os valores:",valorDeA," é o maior que o valor de B."  )
+if(valorDeB > valorDeA):
+    print("Dado os valores:",valorDeB," é o maior que o valor de A."  )
+if(valorDeB == valorDeA):
+    print("Dado os valores, A e B são iguais."  )
