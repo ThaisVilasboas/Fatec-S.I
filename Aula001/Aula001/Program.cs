0188da7d3c7840246019858336d87b8016ea5155
ThaisVilasboas <144953802+ThaisVilasboas@users.noreply.github.com>
2024-08-21 20:28:54 -0300

    Add files via upload

---
+++ Aula001/Aula001/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula001
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float Nota1 = 0, Nota2 = 0, Media = 0;
+
+            Console.Write($"Digite o valor da sua primeira nota: "); 
+            Nota1 = float.Parse(Console.ReadLine());
+
+            Console.Write($"Digite o valor da sua segunda nota: ");
+            Nota2 = float.Parse(Console.ReadLine());
+
+            Media = (Nota1 + Nota2) / 2;
+            Console.Write($"Sua média final é:{Media} ");
+            
+            Console.ReadKey();
+        }
+    }
+}
